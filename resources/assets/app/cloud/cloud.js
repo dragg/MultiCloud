6f129a795c4b0e253e7be4073a03ad7f4ddeb887
Zhidenko Nikolay <dragg.ko@gmail.com>
2015-04-19 14:26:56 +0300

    Init change actions toolbar

--- resources/assets/app/cloud/cloud.js
+++ resources/assets/app/cloud/cloud.js
@@ -13,44 +13,96 @@
         var vm = this,
             cloudId = $stateParams.cloudId;
 
-        vm.activate = activate;
-        vm.fetch = fetch;
-        vm.back = back;
         vm.title = 'cloud';
         vm.contents = [];
         vm.path = $stateParams.path;
-        vm.changeDirectory = changeDirectory;
-        vm.download = download;
+        vm.isSelect = false;
+        vm.selectedContents = [];
+        vm.isSelectFile = false;
+        vm.isSelectFolder = false;
+        vm.isMulti = false;
 
-        activate();
+        //vm.activate = init;
+        vm.select = select;
+        vm.fetch = fetch;
+        vm.back = back;
+        vm.openFolder = changeDirectory;
+        vm.download = download;
+        vm.rename = rename;
+        vm.remove = remove;
+        vm.properties = properties;
+        vm.move = move;
+
+
+        init();
 
         ////////////////
 
-        function activate() {
+        function init() {
             fetch(vm.path);
         }
 
-        function fetch(path) {
+        function select(path) {
+            var index = vm.selectedContents.indexOf(path);
+            if (index === -1) {
+                vm.selectedContents.push(path);
+            } else {
+                vm.selectedContents.splice(index, 1);
+            }
 
-            return Content.fetch(cloudId, path.replace(/\//g, '\\')).then(function(data) {
+            setSelectedState();
+        }
+
+        function setSelectedState() {
+            var lengthSelectedContents = vm.selectedContents.length;
+            vm.isSelect = lengthSelectedContents > 0;
+            vm.isMulti = lengthSelectedContents > 1;
+            vm.isFile = lengthSelectedContents === 1 && vm.selectedContents[0].is_dir === false;
+            vm.isFolder = lengthSelectedContents === 1 && vm.selectedContents[0].is_dir === true;
+        }
+
+        function fetch(path) {
+            return Content.fetch(cloudId, path.replace(/\//g, '\\')).then(function (data) {
                 vm.contents = data;
                 vm.path = path;
             });
         }
 
-        function changeDirectory(path) {
+        function openFolder(path) {
             $state.go('clouds.cloud', {cloudId: cloudId, path: path});
         }
 
-        function back() {
-            changeDirectory(vm.path.substring(0, vm.path.lastIndexOf('\/')));
+        function changeDirectory() {
+            openFolder(vm.selectedContents[0].name);
         }
 
-        function download(path) {
-            return Content.fetch(cloudId, path.replace(/\//g, '\\')).then(function(data) {
+        function back() {
+            openFolder(vm.path.substring(0, vm.path.lastIndexOf('\/')));
+        }
+
+        function download() {
+            var path = vm.selectedContents[0].name;
+
+            return Content.fetch(cloudId, path.replace(/\//g, '\\')).then(function (data) {
                 window.open(data[0]);
                 return data;
             });
+        }
+
+        function rename() {
+            console.log('rename');
+        }
+
+        function remove() {
+            console.log('remove');
+        }
+
+        function properties() {
+            console.log('properties');
+        }
+
+        function move() {
+            console.log('move');
         }
     }
 })();
