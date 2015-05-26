e93107beec0051f574dd2db26a6e5c2049e90c01
Zhidenko Nikolay <dragg.ko@gmail.com>
2015-05-26 23:52:12 +0300

    Add model Task for moving files

---
+++ database/migrations/2015_05_26_204043_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tasks', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->integer('status');
+            $table->dateTime('start');
+            $table->dateTime('end');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::dropIfExists('tasks');
+	}
+
+}
