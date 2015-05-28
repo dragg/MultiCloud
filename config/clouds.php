850a4965c3878eb295e29c2cab6d12b28429264a
Zhidenko Nikolay <dragg.ko@gmail.com>
2015-05-28 19:18:26 +0300

    Move cloud config to environment

--- config/clouds.php
+++ config/clouds.php
@@ -8,18 +8,18 @@
 
 return [
     'yandex_disk' => [
-        'id'        => 'c55932709fa041999ab384d157dabf05',
-        'password'  => '7e7f2b202a674bd4b1f8541c3c706843'
+        'id'        => env('YANDEX_DISK_ID'),
+        'password'  => env('YANDEX_DISK_PASSWORD'),
     ],
 
     'dropbox' => [
-        'key'       => 'uep1augn0ilzl81',
-        'secret'    => '2jud9hnfzfyhflh'
+        'key'       => env('DROPBOX_KEY'),
+        'secret'    => env('DROPBOX_SECRET'),
     ],
 
     'google_drive' => [
-        'name'      => 'MultiCloudThesis',
-        'id'        => '353177346510-55osd15jkh103uq7ekn3gap3ep00d3q4.apps.googleusercontent.com',
-        'secret'    => 'zxe-f9WK884_M5imbnfKm02P',
+        'name'      => env('GOOGLE_DRIVE_NAME'),
+        'id'        => env('GOOGLE_DRIVE_ID'),
+        'secret'    => env('GOOGLE_DRIVE_SECRET'),
     ],
 ];
